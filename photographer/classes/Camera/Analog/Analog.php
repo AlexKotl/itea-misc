b3bc33e9988733b8fec2a226cfb91b30a5517d4f
Alex <slicer256@gmail.com>
2017-09-15 10:42:40 +0300

    added analog camera class

---
+++ photographer/classes/Camera/Analog/Analog.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace Camera;
+
+    class Analog extends \Camera {
+
+        public function shot() {
+            echo "Shot was taken. " . PHP_EOL;
+        }
+
+        public function displayShots() {
+            echo "Opening film...";
+
+            throw new Exception("You've light a film!!! Never open it on the light.");
+        }
+
+    }
+
+?>
--- photographer/index.php
+++ photographer/index.php
@@ -3,9 +3,20 @@
     include("interfaces/CameraInterface.php");
     include("classes/Camera/Camera.php");
     include("classes/Camera/Digital/Digital.php");
+    include("classes/Camera/Analog/Analog.php");
 
     $digitalCamera = new Camera\Digital();
+    $analogCamera = new Camera\Analog();
 
-    $digitalCamera->shot();
+    echo "Photographer with DIGITAL camera taking shots: " . PHP_EOL;
+
+    for ($i = 1; $i <= 20; $i++) {
+        $digitalCamera->shot();
+    }
+
+    $digitalCamera->displayShots();
+
+    echo PHP_EOL . PHP_EOL;
+
 
 ?>
