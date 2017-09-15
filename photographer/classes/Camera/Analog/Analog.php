45a374fa9de086db8a33c5417b1e9950922287b7
Alex <slicer256@gmail.com>
2017-09-15 11:03:56 +0300

    analog camera fails simulation

--- photographer/classes/Camera/Analog/Analog.php
+++ photographer/classes/Camera/Analog/Analog.php
@@ -5,11 +5,27 @@
     class Analog extends \Camera {
 
         public function shot() {
-            echo "Shot was taken. " . PHP_EOL;
+            $this->setAperture(rand(14,160) / 10);
+            $this->setShutter(rand(1,40) * 10);
+
+            $this->shots[] = md5(rand());
+            echo "Shot was taken: Aperture {$this->aperture}, Shutter {$this->shutter} " . PHP_EOL;
+        }
+
+        public function setAperture($aperture) {
+            // sometimes hardware failed to work
+            if (rand() % 7 === 0) {
+                throw new \Exception("Cannot set aperture");
+            }
+            else {
+                $this->aperture = $aperture;
+            }
+
+
         }
 
         public function displayShots() {
-            echo "Opening film...";
+            echo "Opening film... ";
 
             throw new \Exception("You've light a film!!! Never open it on the light.");
         }
